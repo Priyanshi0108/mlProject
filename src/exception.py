17245c9e59adcc1e876d0ed3edef46e174b1d15f
Priyanshi0108 <singhamanrajpoot2007@gmail.com>
2026-07-11 10:26:24 +0530

    add typo

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 
 
@@ -10,14 +11,20 @@ def error_message_details(error,error_detail:sys):
     return error_message    
     
 
-
-class CustomException(Exception):    
-    def __inti__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_details(error_message,error_detail)
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(str(error_message))
+        self.error_message = error_message_details(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message    
-    
+        return self.error_message
 
 
+
+if __name__ == "__main__":
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e,sys)
